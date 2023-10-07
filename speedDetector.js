34aa8dbb42f4887e6a5c5c9c3a2fbab02b0b4bbd
SidraOsman <osmansidra4@gmail.com>
2023-10-07 22:07:47 +0300

    First Commit

---
+++ speedDetector.js
@@ -0,0 +1,14 @@
+function speedDetector(speed){
+    if (speed <= 70) {
+        console.log("Okay")
+    } else {
+        let points = (speed - 70)/5 ;
+        if (points > 12) {
+            console.log("License Suspended");
+        }else {
+            console.log(points);}
+        
+    }
+}
+
+speedDetector(125);
